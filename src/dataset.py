fda31d1e37a5df69d069896bfd6c18cf36db86b4
Yan <oyyfcy@outlook.com>
2024-08-30 21:22:17 +0800

    test12

--- src/dataset.py
+++ src/dataset.py
@@ -9,7 +9,8 @@ if torch.cuda.is_available():
     torch.set_default_tensor_type('torch.cuda.FloatTensor')
 else:
     torch.set_default_tensor_type('torch.FloatTensor')
-    
+
+
 ############################################################################################
 # This file provides basic processing script for the multimodal datasets we use. For other
 # datasets, small modifications may be needed (depending on the type of the data, etc.)
@@ -19,7 +20,7 @@ else:
 class Multimodal_Datasets(Dataset):
     def __init__(self, dataset_path, data='mosei_senti', split_type='train', if_align=False):
         super(Multimodal_Datasets, self).__init__()
-        dataset_path = os.path.join(dataset_path, data+'_data.pkl' if if_align else data+'_data_noalign.pkl' )
+        dataset_path = os.path.join(dataset_path, data + '_data.pkl' if if_align else data + '_data_noalign.pkl')
         dataset = pickle.load(open(dataset_path, 'rb'))
 
         # These are torch tensors
@@ -29,33 +30,41 @@ class Multimodal_Datasets(Dataset):
         self.audio[self.audio == -np.inf] = 0
         self.audio = torch.tensor(self.audio).cpu().detach()
         self.labels = torch.tensor(dataset[split_type]['labels'].astype(np.float32)).cpu().detach()
-        
+
         # Note: this is STILL an numpy array
         self.meta = dataset[split_type]['id'] if 'id' in dataset[split_type].keys() else None
-       
+
         self.data = data
-        
-        self.n_modalities = 3 # vision/ text/ audio
+
+        self.n_modalities = 3  # vision/ text/ audio
+
     def get_n_modalities(self):
         return self.n_modalities
+
     def get_seq_len(self):
         return self.text.shape[1], self.audio.shape[1], self.vision.shape[1]
+
     def get_dim(self):
         return self.text.shape[2], self.audio.shape[2], self.vision.shape[2]
+
     def get_lbl_info(self):
         # return number_of_labels, label_dim
         return self.labels.shape[1], self.labels.shape[2]
+
     def __len__(self):
         return len(self.labels)
+
     def __getitem__(self, index):
         X = (index, self.text[index], self.audio[index], self.vision[index])
         Y = self.labels[index]
-        META = (0,0,0) if self.meta is None else (self.meta[index][0], self.meta[index][1], self.meta[index][2])
+        META = (0, 0, 0) if self.meta is None else (self.meta[index][0], self.meta[index][1], self.meta[index][2])
         if self.data == 'mosi':
-            META = (self.meta[index][0].decode('UTF-8'), self.meta[index][1].decode('UTF-8'), self.meta[index][2].decode('UTF-8'))
+            META = (self.meta[index][0].decode('UTF-8'), self.meta[index][1].decode('UTF-8'),
+                    self.meta[index][2].decode('UTF-8'))
         if self.data == 'iemocap':
             Y = torch.argmax(Y, dim=-1)
-        return X, Y, META        
+        return X, Y, META
+
 
 class PseudolabelMultimodalDataset(Dataset):
     def __init__(self, dataset_path, data='mosei_senti', split_type='train', if_align=False, labeled_ratio=0.5):
@@ -94,6 +103,7 @@ class PseudolabelMultimodalDataset(Dataset):
             self.labels = self.labels.cpu().float()
 
             # Shuffle the data
+            perm = perm.to(self.vision.device)
             perm = torch.randperm(len(self.labels))
             self.vision = self.vision[perm]
             self.text = self.text[perm]
